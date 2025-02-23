b92bf6702d35c83ec736832b0269ab1be2199bae
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-02-23 13:14:28 +0500

    Add method to make code
    method to generate the random code from computer is add

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,33 @@
 class Board
+  @@store_code = []
+  def make_the_code
+    4.times do
+      guess = rand 
+      if guess <= 0.3
+        @@store_code.push("🔴")
+      elsif guess >= 0.5 && guess < 0.8
+        @@store_code.push("🟢")
+      elsif guess >= 0.8 && guess < 0.90
+        @@store_code.push("🔵")
+      else
+        @@store_code.push("🟠")
+      end
+    end
+    p @@store_code
+  end
+  
 end
 
 board = Board.new
-puts "⚪️"
-puts "⚫"
-puts "🔴"
-puts "🟢"
-puts "🔵"
-puts "🟠"
+board.make_the_code
+# puts "⚪️"
+# puts "⚫"
+
+# puts "🔴"
+# puts "🟢"
+# puts "🔵"
+# puts "🟠"
+
+# 5.times do
+#   p rand
+# end
